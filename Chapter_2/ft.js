34e5dc0322f7ae35985a88bd8bda3cf2641a7a29
Cassandra Cruz <60939617+cassandracruz@users.noreply.github.com>
2020-12-08 19:10:20 -0600

    Create ft.js

---
+++ Chapter_2/ft.js
@@ -0,0 +1,20 @@
+/*    JavaScript 6th Edition
+ *    Chapter 2
+ *    Chapter case
+
+ *    Fan Trick Fine Art Photography
+ *    Variables and functions
+ *    Author: Cassie
+ *    Date:  2020-08-12
+
+ *    Filename: ft.js
+ */
+
+
+// global variables
+var photographerCost = 0;
+var totalCost = 0;
+var memoryBook = false;
+var reproductionRights = false;
+
+
