0c686406bad04aa1ea965051d3ff2d1d173ecc55
Cassandra Cruz <60939617+cassandracruz@users.noreply.github.com>
2020-12-08 19:24:14 -0600

    Update ft.js
    Added resetForm function.  Sets all form field values to defaults when page is reloaded.

--- Chapter_2/ft.js
+++ Chapter_2/ft.js
@@ -4,7 +4,7 @@
 
  *    Fan Trick Fine Art Photography
  *    Variables and functions
- *    Author: Cassie
+ *    Author: Cassandra CruzRamirez
  *    Date:  2020-08-12
 
  *    Filename: ft.js
@@ -17,4 +17,14 @@ var totalCost = 0;
 var memoryBook = false;
 var reproductionRights = false;
 
+//sets all form field values to defaults
+function resetForm(){
+	document.getElementById("photognum").value = 1;
+	document.getElementById("photoghrs").value = 2;
+	document.getElementById("membook").checked = memoryBook;
+	document.getElementById("reprodrights").checked = reproductionRights;
+	document.getElementById("distance").value = 0;
+}
 
+//resets form when page is reloaded
+window.addEventListener("load", resetForm, false);
