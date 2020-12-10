356d297e7dc739e3a3664caab47213173f15c0fa
Cassandra Cruz <cassandracruz68@gmail.com>
2020-12-09 18:19:43 -0600

    added tt.js

---
+++ Chapter_3/tt.js
@@ -0,0 +1,11 @@
+/*    JavaScript 6th Edition
+ *    Chapter 3
+ *    Chapter case
+
+ *    Tipton Turbines
+ *    Variables and functions
+ *    Author: Cassandra CruzRamirez
+ *    Date:   2020/09/12
+
+ *    Filename: tt.js
+ */
