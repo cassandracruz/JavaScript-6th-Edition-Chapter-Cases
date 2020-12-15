1a4f185f62a2cb9ffff6fba8ef8b94e8c573e932
Cassandra Cruz <60939617+cassandracruz@users.noreply.github.com>
2020-12-15 16:24:42 -0600

    Added tuba.js

---
+++ Chapter_4/tuba.js
@@ -0,0 +1,11 @@
+/*    JavaScript 6th Edition
+ *    Chapter 4
+ *    Chapter case
+
+ *    Tuba Farm Equipment
+ *    Variables and functions
+ *    Author: Cassandra CruzRamirez
+ *    Date:   2020-12-14
+
+ *    Filename: tuba.js
+ */
