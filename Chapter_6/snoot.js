c70c55deb266ce174697ccfdf5b5b72c5f353112
Cassandra Cruz <60939617+cassandracruz@users.noreply.github.com>
2021-01-04 17:03:31 -0600

    Create snoot.js

---
+++ Chapter_6/snoot.js
@@ -0,0 +1,14 @@
+/* JavaScript 6th Edition
+ * Chapter 6
+ * Chapter case
+ * Snoot Flowers
+ * Functions
+ * Author: Cassandra CruzRamirez
+ * Date:  2021-01-04  
+
+ *    Filename: snoot.js
+ */
+
+"use strict"; // interpret document contents in JavaScript strict   mode
+
+}
